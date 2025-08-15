f1dcc8ddffbc4cda594ff6bfa036f86be02aa004
Harman Goei <5001432+hrgui@users.noreply.github.com>
2025-08-14 19:01:52 -0700

    feat: storybook changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,12 @@
-
+process.env.STORYBOOK = '1';
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-docs"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-docs'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
 };
 export default config;
